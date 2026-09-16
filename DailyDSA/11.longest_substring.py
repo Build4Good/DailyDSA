a407f3362bb8ed197972a7be59e726c17a57cf74
Build4Good <Abhishekchaudhary619@gmail.com>
2026-09-16 08:56:09 +0530

    Moved window to set for quick insert and delete

--- DailyDSA/11.longest_substring.py
+++ DailyDSA/11.longest_substring.py
@@ -14,14 +14,14 @@ Example:
 def length_of_longest_substring(s: str) -> int:
     l=0 
     r=0
-    window=[]
+    window=set()
     max_len=0
     while r<len(s):
 
         while s[r] in window:
             window.remove(s[l])
             l+=1
-        window.append(s[r])
+        window.add(s[r])
         max_len = max(max_len, r - l+1)
         r+=1
     
